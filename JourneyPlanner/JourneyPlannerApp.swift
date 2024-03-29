7f96ea5cdc23d3fb5acc598e98a9720ac325d0df
Simon Lawrence <simon.h.lawrence@gmail.com>
2024-03-29 13:25:28 +0000

    Initial Commit

---
+++ JourneyPlanner/JourneyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyPlannerApp.swift
+//  JourneyPlanner
+//
+//  Created by Simon Lawrence on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct JourneyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
